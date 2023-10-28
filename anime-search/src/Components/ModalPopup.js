8856837c51655f062e1b3006e71683b7600416e8
Sam Elliott <soe1@uakron.edu>
2023-10-28 15:49:16 -0400

    added a little code to move the modal around a bit, I think I have the hang of it now. Next step is going to be doing a lot of CSS styling on the popup itself now that I know I can implement it

--- anime-search/src/Components/ModalPopup.js
+++ anime-search/src/Components/ModalPopup.js
@@ -6,14 +6,14 @@ export default function ModalPopup({ open, onClose, title, image, synopsis }) {
 
     return (
         <div className='text-6xl flex justify-evenly items-center 
-        shadow bg-white border-white border-2 rounded-xl
-        absolute w-62 top-70 left-[calc(50%-125px)] bottom-70'>
+        shadow bg-white border-black border-2 rounded-xl
+        absolute top-20 left-10 bottom-70 w-13 z-9999'>
             Modal
             <button onClick={onClose}>Close</button>
             <br></br>
             <p className='text-3xl'> {title} </p>
             <img src={image}></img>
-            <p className='text-md'>{synopsis}</p>
+            <p className='text-sm'>{synopsis}</p>
         </div>
     )
 }
